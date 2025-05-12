5d10b68e2cae6572bf0e168b95ccfb261b18c122
Nayeem Abdul Qaiyum <qaiyum.1416@gmail.com>
2025-05-12 15:56:42 +0600

    condition

---
+++ css/script.js
@@ -0,0 +1,29 @@
+let age = 28;
+let age1 = 60;
+let nationality = "Bangladeshi , India";
+let gender = 1
+
+if(age>=18){
+    if(nationality = "Bangladeshi")
+    {
+        if(gender = 1){
+            console.log("Male")
+            console.log("Bangladeshi")
+            if(age1>=35){
+                if(nationality = "India"){
+                    if(gender = 0){
+                        console.log("Male")
+                        console.log("India")
+                    }
+                    else{
+                        console.log("Female")
+                        console.log("India")
+                    }
+                }
+            }
+        }
+    }
+}
+else{
+        console.log("Unidentified")
+    }
