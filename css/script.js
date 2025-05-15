a484a7e44f5c724563d80278c6ac23deee4e6e60
Nayeem Abdul Qaiyum <qaiyum.1416@gmail.com>
2025-05-16 00:24:02 +0600

    loop

--- css/script.js
+++ css/script.js
@@ -3,27 +3,56 @@ let age1 = 60;
 let nationality = "Bangladeshi , India";
 let gender = 1
 
-if(age>=18){
-    if(nationality = "Bangladeshi")
-    {
-        if(gender = 1){
-            console.log("Male")
-            console.log("Bangladeshi")
-            if(age1>=35){
-                if(nationality = "India"){
-                    if(gender = 0){
-                        console.log("Male")
-                        console.log("India")
-                    }
-                    else{
-                        console.log("Female")
-                        console.log("India")
-                    }
-                }
-            }
-        }
-    }
+// condition
+// if(age>=18){
+//     if(nationality = "Bangladeshi")
+//     {
+//         if(gender = 1){
+//             console.log("Male")
+//             console.log("Bangladeshi")
+//             if(age1>=35){
+//                 if(nationality = "India"){
+//                     if(gender = 0){
+//                         console.log("Male")
+//                         console.log("India")
+//                     }
+//                     else{
+//                         console.log("Female")
+//                         console.log("India")
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+// else{
+//         console.log("Unidentified")
+//     }
+
+// loop
+
+// for loop - Namota
+let i=prompt("Enter a Number:");
+
+for(let x=1 ; x<=10; x++){
+    console.log("Number - ",x*i);
 }
-else{
-        console.log("Unidentified")
-    }
+
+// while loop
+
+// let i = 0;
+
+// while(i<5){
+//     console.log("Test while loop - ", "count "+ i);
+//     i++;
+// }
+
+// do while loop
+
+// let y=prompt("Enter a number");
+
+// do{
+//     console.log("Do while", y)
+//     y++;
+// }
+// while(y<10)
