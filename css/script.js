966c184c3e1d7babbc83874a4191ed68b8223d53
Nayeem Abdul Qaiyum <qaiyum.1416@gmail.com>
2025-05-16 17:06:10 +0600

    practice

--- css/script.js
+++ css/script.js
@@ -29,16 +29,17 @@ let gender = 1
 //         console.log("Unidentified")
 //     }
 
-// loop
+
+//............................................... loop
 
 // for loop - Namota
-let i=prompt("Enter a Number:");
+// let i=prompt("Enter a Number:");
 
-for(let x=1 ; x<=10; x++){
-    console.log("Number - ",x*i);
-}
+// for(let x=1 ; x<=10; x++){
+//     console.log("Number - ",x*i);
+// }
 
-// while loop
+//..............................................while loop
 
 // let i = 0;
 
@@ -47,7 +48,7 @@ for(let x=1 ; x<=10; x++){
 //     i++;
 // }
 
-// do while loop
+//.............................................do while loop
 
 // let y=prompt("Enter a number");
 
@@ -56,3 +57,31 @@ for(let x=1 ; x<=10; x++){
 //     y++;
 // }
 // while(y<10)
+
+
+
+// ................................condition with loop
+var x=prompt("Enter a number");
+
+var a="Dhaka";
+var b="Bandarban";
+var c="Sylhet";
+
+if(x<=10){
+    console.log("Customer review - ")
+}else if(x<=20){
+       for(let i=0; i<5; i++){
+        console.log("Hello from "+ a,i)
+    } 
+}else if(x<=30){
+        for(let i=0; i<8; i++){
+        console.log("Hello from "+b,i)
+    }
+}
+else if(x<=40){
+        for(let i=0; i<10; i++){
+        console.log("Hello from "+ c,i)
+    }
+}else{
+    console.log("Input a valid number under 40")
+}
